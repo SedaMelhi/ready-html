007967e52f0c69013840caa93c1637507e852574
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-05-23 14:31:26 +0300

    .

--- ready-html/js/app.js
+++ ready-html/js/app.js
@@ -9,4 +9,24 @@ window.addEventListener('load', () => {
     wrapper: '.wrapper',
     content: '.content',
   });
+  const bags = document.querySelectorAll('.small');
+  const texts = document.querySelectorAll('.opacity');
+  let count = 0;
+  bags.forEach((bag) =>
+    bag.addEventListener('click', () => {
+      count++;
+      texts.forEach((text) => {
+        text.innerHTML =
+          count == 1
+            ? 'Первый найден!'
+            : count == 2
+            ? 'Второй найден!'
+            : count == 3
+            ? 'Третий найден!'
+            : 'Найдены все! Я вижу ты опытная Леди Баг. Но согласись, эти ошибки смотрелись довольно мило. Однако пора двигаться дальше. Тут будет ребус. ';
+        text.classList.add('opacity_none');
+      });
+      bag.remove();
+    }),
+  );
 });
