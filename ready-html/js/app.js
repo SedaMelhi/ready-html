3009c2aeb096b8358721d0607b44d80aa758f8f6
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-05-23 14:06:33 +0300

    .

--- ready-html/js/app.js
+++ ready-html/js/app.js
@@ -1,8 +1,12 @@
-window.addEventListener('scroll', e => {
-	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
-})
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-	wrapper: '.wrapper',
-	content: '.content'
-})
+window.addEventListener('load', () => {
+  document.querySelector('.load').remove();
+  document.querySelector('.content').classList.remove('hidden');
+  window.addEventListener('scroll', (e) => {
+    document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`); // Update method
+  });
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+  });
+});
